929e00a70354f15d7ec124d18e8d8a44cd80acee
sakibalamin <sakibalamin162@gmail.com>
2020-08-13 19:01:32 -0700

    category create route created

--- back-end/src/routes/admin/auth.js
+++ back-end/src/routes/admin/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/user');
 const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validators/auth');
 const { signup, signin } = require('../../controller/admin/auth');
 
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 const env = require('dotenv');
 env.config();
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-app.use(express.json());
 
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
+const categoryRoutes = require('./routes/category');
 
 mongoose.connect(
     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.ssrhj.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
     {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
 ).then(() => {
-    console.log('database connected successfully!');
+    console.log('mongodb cloud database connected successfully!');
 });
 
+app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
+app.use('/api', categoryRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port: ${process.env.PORT}`);
