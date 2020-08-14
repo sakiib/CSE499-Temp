38f9089d0702cb7c4391056abfc391943c6eefc4
sakibalamin <sakibalamin162@gmail.com>
2020-08-13 17:47:57 -0700

    api request validation done

--- back-end/src/routes/admin/auth.js
+++ back-end/src/routes/admin/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/user');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validators/auth');
 const { signup, signin } = require('../../controller/admin/auth');
 
-router.post('/admin/signin', signin);
-router.post('/admin/signup', signup);
+router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
+router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 
 module.exports = router;
--- back-end/src/routes/auth.js
+++ back-end/src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth');
 const { signup, signin } = require('../controller/auth');
 
-router.post('/signin', signin);
-router.post('/signup', signup);
+router.post('/signup', validateSignupRequest, isRequestValidated, signup);
+
+router.post('/signin', validateSigninRequest, isRequestValidated, signin);
 
 module.exports = router;
