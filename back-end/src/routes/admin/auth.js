d6bd34a581c3b3fb8ce7b293459ebb07af72492d
sakibalamin <sakibalamin162@gmail.com>
2020-08-15 23:38:22 -0700

    added routing details

--- back-end/src/routes/admin/auth.js
+++ back-end/src/routes/admin/auth.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../../validators/auth');
 const { signup, signin } = require('../../controller/admin/auth');
 
+// @route POST /api/admin/signin
+// @desc signin admin
+// @access Admin
 router.post('/admin/signin', validateSigninRequest, isRequestValidated, signin);
+
+// @route POST /api/admin/signup
+// @desc signin admin
+// @access Admin
 router.post('/admin/signup', validateSignupRequest, isRequestValidated, signup);
 
 module.exports = router;
