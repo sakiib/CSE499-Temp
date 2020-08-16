762c37775b5ee495ad385b0509ca43eb6d28b89b
sakibalamin <sakibalamin162@gmail.com>
2020-08-15 23:47:57 -0700

    added create category details

--- back-end/src/routes/category.js
+++ back-end/src/routes/category.js
@@ -3,6 +3,9 @@ const { addCategory, getCategories } = require('../controller/category');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
 const router = express.Router();
 
+// @route POST /api/category/create
+// @desc POST create a new category
+// @access Admin
 router.post('/category/create', requireSignin, adminMiddleware, addCategory);
 router.get('/category/getcategory', getCategories);
 
