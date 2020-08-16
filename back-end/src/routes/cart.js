3bb0266ac3f89967ab5f208a9b2e224bc93a6461
sakibalamin <sakibalamin162@gmail.com>
2020-08-15 23:46:06 -0700

    added addtocart details

--- back-end/src/routes/cart.js
+++ back-end/src/routes/cart.js
@@ -3,6 +3,10 @@ const { addItemToCart } = require('../controller/cart');
 const { requireSignin, userMiddleware } = require('../common-middleware');
 const router = express.Router();
 
+// @route POST /api/user/cart/addtocart
+// @desc POST add a product to cart
+// @access User
+
 router.post('/user/cart/addtocart', requireSignin, userMiddleware, addItemToCart);
 
 module.exports = router;
