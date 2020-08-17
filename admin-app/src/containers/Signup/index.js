7e2a83cbaf9fabae51af3d52ffb40c0a8eac0381
sakibalamin <sakibalamin162@gmail.com>
2020-08-17 16:44:45 -0700

    admin app layout setup done

--- admin-app/src/containers/Signup/index.js
+++ admin-app/src/containers/Signup/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Layout from '../../components/Layout';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import Input from '../../components/UI/Input';
 
 /**
 * @author SakibAlamin
@@ -7,7 +10,54 @@ import React from 'react';
 
 const Signup = (props) => {
   return(
-    <div> Signup </div>
+    <Layout> 
+        <Container> 
+            <Row style={{marginTop: '50px'}}> 
+                <Col md={{span: 6, offset: 3}}> 
+                    <Form>
+                        <Row>
+                            <Col md={6}>
+                                <Input 
+                                    label="First Name"
+                                    placeholder="First Name"
+                                    value=""
+                                    type="text"
+                                    onChange={() => {}}
+                                />
+                            </Col>
+                            <Col md={6}>
+                                <Input 
+                                    label="Last Name"
+                                    placeholder="Last Name"
+                                    value=""
+                                    type="text"
+                                    onChange={() => {}}
+                                />
+                            </Col>
+                        </Row>
+                        <Input 
+                            label="Email"
+                            placeholder="Email"
+                            value=""
+                            type="email"
+                            onChange={() => {}}
+                        />
+
+                        <Input 
+                            label="Password"
+                            placeholder="Password"
+                            value=""
+                            type="password"
+                            onChange={() => {}}
+                        />
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    </Layout>
    )
  };
 
