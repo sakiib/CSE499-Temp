4257f5358b9be220a6f11d9cc09d3dcc4a5eba43
sakibalamin <sakibalamin162@gmail.com>
2020-08-17 12:38:03 -0700

    added google fonts & Jumbotron

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Jumbotron } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <Jumbotron> 
+        <h1> Admin Dashboard </h1>
+      </Jumbotron>
     </div>
   );
 }
