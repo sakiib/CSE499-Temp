2c350d2febf1660c7307b92bc1b8673d0d084b64
sakibalamin <sakibalamin162@gmail.com>
2020-08-19 13:26:26 -0700

    action login success

--- admin-app/src/actions/auth.actions.js
+++ admin-app/src/actions/auth.actions.js
@@ -4,15 +4,27 @@ import axios from "../helpers/axios";
 export const login = (user) => {
     return async (dispatch) => {
 
+        dispatch({ type: authConstants.LOGIN_REQUEST });
         const res = await axios.post(`/admin/signin`, {
             ...user
         });
 
-        dispatch({ 
-            type: authConstants.LOGIN_REQUEST, 
-            payload: {
-                ...user
+        if (res.status === 200) {
+            const { token, user } = res.data;
+            localStorage.setItem('token', token);
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token, user
+                }
+            });
+        } else {
+            if (res.status === 400) {
+                dispatch({
+                    type: authConstants.LOGIN_FAILURE,
+                    payload: { error: res.data.error }
+                });
             }
-        });
+        }
     }
 }
