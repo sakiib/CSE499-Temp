05f1f4594bdbc7fab457d7df2f04aca946c5efe6
sakibalamin <sakibalamin162@gmail.com>
2020-08-22 13:17:52 -0700

    signup added

--- admin-app/src/actions/auth.actions.js
+++ admin-app/src/actions/auth.actions.js
@@ -30,6 +30,33 @@ export const login = (user) => {
     }
 }
 
+// export const signup = (user) => {
+//     return async (dispatch) => {
+
+//         dispatch({ type: authConstants.LOGIN_REQUEST });
+//         const res = await axios.post(`/admin/signup`, {
+//             ...user
+//         });
+
+//         if (res.status === 200) {
+//             const { message } = res.data;
+//             dispatch({
+//                 type: authConstants.LOGIN_SUCCESS,
+//                 payload: {
+//                     message
+//                 }
+//             });
+//         } else {
+//             if (res.status === 400) {
+//                 dispatch({
+//                     type: authConstants.LOGIN_FAILURE,
+//                     payload: { error: res.data.error }
+//                 });
+//             }
+//         }
+//     }
+// }
+
 export const isUserLoggedIn = () => {
     return async dispatch => {
         const token = localStorage.getItem('token');
--- admin-app/src/containers/Signup/index.js
+++ admin-app/src/containers/Signup/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/Layout';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 import Input from '../../components/UI/Input';
 import { Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { signup } from '../../actions';
 
 /**
 * @author SakibAlamin
@@ -11,50 +12,66 @@ import { useSelector } from 'react-redux';
 **/
 
 const Signup = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+  
+  const userSignup = (e) => {
+    e.preventDefault();
+    const user = {
+        firstName, lastName, email, password
+    }
+    dispatch(signup(user));
+  };
+
   if (auth.authenticate) {
     return <Redirect to={`/`}/>
   }
+
   return(
     <Layout> 
         <Container> 
             <Row style={{marginTop: '50px'}}> 
                 <Col md={{span: 6, offset: 3}}> 
-                    <Form>
+                    <Form onSubmit={userSignup}>
                         <Row>
                             <Col md={6}>
                                 <Input 
                                     label="First Name"
                                     placeholder="First Name"
-                                    value=""
+                                    value={firstName}
                                     type="text"
-                                    onChange={() => {}}
+                                    onChange={(e) => setFirstName(e.target.value)}
                                 />
                             </Col>
                             <Col md={6}>
                                 <Input 
                                     label="Last Name"
                                     placeholder="Last Name"
-                                    value=""
+                                    value={lastName}
                                     type="text"
-                                    onChange={() => {}}
+                                    onChange={(e) => setLastName(e.target.value)}
                                 />
                             </Col>
                         </Row>
                         <Input 
                             label="Email"
                             placeholder="Email"
-                            value=""
+                            value={email}
                             type="email"
-                            onChange={() => {}}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
 
                         <Input 
                             label="Password"
                             placeholder="Password"
-                            value=""
+                            value={password}
                             type="password"
-                            onChange={() => {}}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                         <Button variant="primary btn-block" type="submit">
                             Submit
